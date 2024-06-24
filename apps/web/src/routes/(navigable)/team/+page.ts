3c026f97c921559ecf0f3ab55bc55e49c6f39b87
Maxime Bele <maxime.bele@gmail.com>
2024-06-25 00:28:43 +0200

    Add experience bar display in character drawer

--- apps/web/src/routes/(navigable)/team/+page.ts
+++ apps/web/src/routes/(navigable)/team/+page.ts
@@ -20,22 +20,6 @@ function classNameToPortrait(className: string) {
 
 export const load = (async ({ parent }) => {
 	const { userID } = await parent();
-/*
-	const channel = DB_CLIENT.channel('supabase_realtime')
-		.on(
-			'postgres_changes',
-			{
-				event: '*',
-				schema: 'public',
-				table: 'playerteam',
-				filter: `playerid=eq.${userID}`,
-		  
-			},
-			(payload) => console.log(payload)
-		)
-		.subscribe();
-
-*/
 	const { data: playerPawns } = await DB_CLIENT.from('playerpawns')
 		.select('*')
 		.eq('playerid', userID);
@@ -53,7 +37,9 @@ export const load = (async ({ parent }) => {
 				attack: pawn.attack,
 				defense: pawn.defense,
 				speed: pawn.speed,
-				fielded: pawn.isinteam
+				fielded: pawn.isinteam,
+				experience: pawn.experience,
+				toNextLevel: pawn.tonextlevel,
 			};
 		}) || [];
 
--- packages/ui/src/lib/components/app/Character/Drawer/Drawer.svelte
+++ packages/ui/src/lib/components/app/Character/Drawer/Drawer.svelte
@@ -3,7 +3,7 @@
 	import Feather from '$lib/assets/itemicon_feather.png';
 	import Sword from '$lib/assets/Itemicon_Equipment_Sword.png';
 	import Star from '$lib/assets/Icon_RankIcon_Star01_l.png';
-	import Shield from "$lib/assets/shield.png"
+	import Shield from '$lib/assets/shield.png';
 	import Health from '$lib/assets/Icon_ColorIcon_Life.png';
 
 	export let hero: {
@@ -19,6 +19,8 @@
 		defense: number;
 		speed: number;
 		fielded: boolean;
+		experience: number;
+		toNextLevel: number;
 	};
 
 	const statsKeys = ['health', 'attack', 'defense', 'speed'];
@@ -33,29 +35,48 @@
 
 	export let action: (id: string) => void;
 	export let disabled = false;
+
+	$: experienceProgress = (hero.experience / hero.toNextLevel) * 100;
+	$: tnl = hero.toNextLevel - hero.experience;
 </script>
 
-<div class=" mx-auto w-full max-w-[422px] p-4">
-	<div class="flex items-center justify-between">
-		<div>
-			<div>
-				Level <span class="text-xl font-semibold text-yellow-500">{hero.level}</span> - {hero.class}
+<div class="mx-auto w-full max-w-[422px] space-y-4 p-4">
+	<div class="grid grid-cols-3">
+		<div class="col-span-2">
+			<div class="text-lg font-medium">
+				{hero.class}
 			</div>
-			<div>
-				<div class="mt-2 flex items-center space-x-0.5">
-					{#each new Array(5) as _, i}
-						{@const isRare = hero.rarity > i}
-						<img src={Star} alt="" class="h-6 w-6 {isRare ? '' : 'grayscale'} " />
-					{/each}
-				</div>
-				<div></div>
+			<div class="mt-1 flex items-center space-x-0.5">
+				{#each new Array(5) as _, i}
+					{@const isRare = hero.rarity > i}
+					<img src={Star} alt="" class="h-4 w-4 {isRare ? '' : 'grayscale'} " />
+				{/each}
 			</div>
 		</div>
+		<div class="space-y-1 text-right">
+			<div>
+				Level <span class="text-xl font-semibold text-yellow-500">{hero.level}</span>
+			</div>
+			<div>
+				<div
+					class="w-full h-2 border border-yellow-500 rounded-lg from-yellow-500"
+					style="background-image: linear-gradient(to right, var(--tw-gradient-from), var(--tw-gradient-from) {experienceProgress}%, transparent {experienceProgress +
+						1}%);"
+				></div>
 
-		<Button on:click={() => action(hero.id)} {disabled}>{hero.fielded ? 'Remove' : 'Use'}</Button>
+				
+			</div>
+			<div class="text-xs">
+				<span class="text-yellow-500">
+					{tnl}
+				</span> exp remaining
+			</div>
+		</div>
 	</div>
-	<div class="mt-6">
-		<div class="text-xs font-medium uppercase">Stats</div>
+	<div class="">
+		<div class="flex items-baseline justify-between">
+			<div class="text-xs font-medium uppercase text-muted-foreground">Stats</div>
+		</div>
 		<div class="grid grid-cols-2 gap-4 mt-2">
 			{#each stats as { key, value }}
 				<div class="flex items-center px-4 py-2 space-x-3 border rounded-lg bg-muted">
@@ -69,5 +90,10 @@
 				</div>
 			{/each}
 		</div>
+	</div>
+	<div class="">
+		<Button class="w-full" on:click={() => action(hero.id)} {disabled}
+			>{hero.fielded ? 'Remove' : 'Use'}</Button
+		>
 	</div>
 </div>
